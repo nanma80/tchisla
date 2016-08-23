c7f816275971b36ac6366d39ab8ad86889cf6d6c
Nan Ma <nma@yammer-inc.com>
2016-08-23 16:55:31 -0700

    one way to search for solutions

--- tchisla/collection.py
+++ tchisla/collection.py
@@ -12,13 +12,13 @@ class Collection():
       print "{}/{}".format(generation_index, generation_limit - 1)
       generation = Generation(generation_index, self.base)
       generation.build()
+    self.sorted_keys = sorted(Solution.registry.keys(), key=lambda x: sympy.N(x))
 
-  def output(self, number_limit=100):
+  def output(self, number_limit=None):
     for solution in self.iter_solution(number_limit):
       print solution.formatted()
 
-  def iter_solution(self, number_limit=100):
-    sorted_keys = sorted(Solution.registry.keys(), key=lambda x: sympy.N(x))
-    for key in sorted_keys:
-      if number_limit is None or key <= number_limit:
+  def iter_solution(self, number_limit=None):
+    for key in self.sorted_keys:
+      if number_limit is None or sympy.N(key) <= number_limit:
         yield Solution.registry[key]
--- test.py
+++ test.py
@@ -1,7 +1,27 @@
 import sympy
 import tchisla as t
 
-collection = t.Collection(6)
+target = 1024
+target_digits = 6
+
+t.Operator.power_limit = 2
+
+collection = t.Collection(7)
 collection.build(5)
-for solution in collection.iter_solution():
-  print sympy.Rational(357)/solution.number
+print len(t.Solution.registry)
+
+for solution1 in collection.iter_solution():
+  if solution1.complexity > 2:
+    continue
+  if sympy.N(solution1.number) < 0.1 ** 10 or sympy.N(solution1.number) > 10.0 ** 10:
+    continue
+  print solution1.number
+  for solution2 in collection.iter_solution():
+    if (
+      sympy.N(solution2.number) < target * 1.1 / sympy.N(solution1.number) and
+      solution1.complexity + solution2.complexity <= target_digits and
+      solution1.number * solution2.number == target
+    ):
+      print "Found solution"
+      print solution1.formatted()
+      print solution2.formatted()
