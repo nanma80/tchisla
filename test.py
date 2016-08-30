afe082e7b4332a1e06d7d3540de8150265e65541
Nan Ma <nma@yammer-inc.com>
2016-08-30 11:13:28 -0700

    Batch solve from both sides

--- test.py
+++ test.py
@@ -1,25 +1,22 @@
 import sympy
 import tchisla as t
 
-target = 1898
-target_digits = 7
+target_pairs = [(804, 6), (867, 7)]
 
-collection = t.Collection(2)
-collection.build(5)
+collection = t.Collection(8)
+collection.build(6)
 print len(t.Solution.registry)
 
-for solution1 in collection.iter_solution():
-  if solution1.complexity > 4:
-    continue
-  if sympy.N(solution1.number) < 0.1 ** 10 or sympy.N(solution1.number) > 10.0 ** 10:
-    continue
-  print solution1.number
-  for solution2 in collection.iter_solution():
-    if (
-      sympy.N(solution2.number) < target * 1.1 / sympy.N(solution1.number) and
-      solution1.complexity + solution2.complexity <= target_digits and
-      solution1.number * solution2.number == target
-    ):
-      print "Found solution"
-      print solution1.formatted()
-      print solution2.formatted()
+for (target, target_digits) in target_pairs:
+  for solution1 in collection.iter_solution():
+    value1 = solution1.number
+    if value1 < 0:
+      continue
+    value2 = sympy.Rational(1) * target / value1
+    if value2 in t.Solution.registry:
+      solution2 = t.Solution.registry[value2]
+      if solution1.complexity + solution2.complexity <= target_digits:
+        print "Found solution for ", target
+        print solution1.formatted()
+        print solution2.formatted()
+        break
