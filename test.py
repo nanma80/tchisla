e3034a4de2caa3e6361738e7e1ab37d89eb137db
Nan Ma <nma@yammer-inc.com>
2016-09-08 10:10:57 -0700

    Handle division case

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sympy
 import tchisla as t
 
-target_pairs = [(804, 6), (867, 7)]
+target_pairs = [(1344, 6)]
 
-collection = t.Collection(8)
-collection.build(6)
+collection = t.Collection(7)
+collection.build(5)
 print len(t.Solution.registry)
 
 for (target, target_digits) in target_pairs:
@@ -16,7 +16,30 @@ for (target, target_digits) in target_pairs:
     if value2 in t.Solution.registry:
       solution2 = t.Solution.registry[value2]
       if solution1.complexity + solution2.complexity <= target_digits:
-        print "Found solution for ", target
+        print "Found solution (*) for ", target
+        print solution1.formatted()
+        print solution2.formatted()
+        break
+    value2 = sympy.Rational(1) * target * value1
+    if value2 > 0 and value2 in t.Solution.registry:
+      solution2 = t.Solution.registry[value2]
+      if solution1.complexity + solution2.complexity <= target_digits:
+        print "Found solution (/) for ", target
+        print solution1.formatted()
+        print solution2.formatted()
+        break
+
+
+for (target, target_digits) in target_pairs:
+  for solution1 in collection.iter_solution():
+    value1 = solution1.number
+    if sympy.N(value1) < 0:
+      continue
+    value2 = sympy.Rational(1) * target - value1
+    if value2 in t.Solution.registry:
+      solution2 = t.Solution.registry[value2]
+      if solution1.complexity + solution2.complexity <= target_digits:
+        print "Found solution (+) for ", target
         print solution1.formatted()
         print solution2.formatted()
         break
