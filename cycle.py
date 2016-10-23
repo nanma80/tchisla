7ba55f5b936403c6a7130094b39c43d1228dc8f0
Nan Ma <nma@yammer-inc.com>
2016-10-22 17:53:49 -0700

    combined cycle

--- cycle.py
+++ cycle.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import sys
-
-filename, digits = sys.argv
 
 os.system("./refresh_cache.py")
-os.system("./range_cheater.py 1 10000 {} {} skip".format(digits, digits))
+os.system("./range_cheater.py 1 10000 1 9 skip")
 os.system("./refresh_cache.py")
-os.system("./wr_checker.py 1 10000 {} > results/wr_checker_{}.txt".format(digits, digits))
+os.system("./wr_checker.py 1 10000 1 9 > results/wr_checker.txt")
